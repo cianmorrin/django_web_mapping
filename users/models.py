d5abc012d8edb1f2212c7017abfed665343378b7
Cian Morrin <c17718445@mytudublin.ie>
2020-10-28 17:02:50 +0000

    update profile model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE())
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, editable=False)
     last_location = models.PointField(
@@ -11,7 +11,7 @@ class Profile(models.Model):
         blank=True,
         null=True,
         default=None)
-    
+
     # Returns the string representation of the model.
     def __str__(self):
         return self.name
