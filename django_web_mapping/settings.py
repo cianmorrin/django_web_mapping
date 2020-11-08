6d38deddf98101fa52a6870dd7d44bf80cad0661
Cian Morrin <c17718445@mytudublin.ie>
2020-11-08 16:24:04 +0000

    .gitignore is now working

--- django_web_mapping/settings.py
+++ django_web_mapping/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-key = Path('key.txt').read_text()
+key = Path('secretkey.txt').read_text()
 SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
