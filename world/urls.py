c3012e4d49d13571fea30f9e468dc01994417d0e
Cian Morrin <c17718445@mytudublin.ie>
2020-12-09 01:58:04 +0000

    change flight url

--- world/urls.py
+++ world/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('countries/', views.flights, name='world-flights'),
+    path('flights/', views.flights, name='world-flights'),
 ]
